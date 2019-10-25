00e3cbcb22cc7eca051703653cee91af9e6fbfef
farhannr <farhan.nazir@axis.com>
2019-10-25 22:56:02 +0200

    Version 1.0 started

---
+++ src/component/printRmaLabel.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Paper } from '@material-ui/core';
+import QRCode from "qrcode.react";
+
+class PrintRmaLabel extends Component {
+    render() {
+        return (
+            <Paper style={{ textAlign: "center", width: "600px", height: "500px", marginLeft: "5px" }}>
+                <div style={{ marginTop: "20px" }}>
+                    <h2>RMA Case Detail</h2>
+                    <h3>RMA ID: {this.props.RMAID}</h3>
+                    <h3>Serial # {this.props.SerialNumber} </h3>
+                    <h3>Prod-Unit: {this.props.ProdUnit}</h3>
+                    <h3>C.O.O: {this.props.Country}</h3>
+                    <br />
+                    <QRCode size={84} value={this.props.RMAID} />
+                </div>
+            </Paper>
+
+
+
+        );
+    }
+}
+
+export default PrintRmaLabel;
+
+//ref={el => (this.componentRef = el)}
---
+++ src/component/rmaForm.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import { TextField, Typography, Button, Paper } from '@material-ui/core';
+import PrintRmaLabel from './printRmaLabel';
+import ReactToPrint from "react-to-print";
+class RmaForm extends Component {
+
+    state = {
+        scanUnit: "",
+        rmaID: "",
+        unitData: []
+    }
+
+    handleOnChange = (e) => {
+        let unit = e.target.value;
+        this.setState({ scanUnit: unit });
+        let res = unit.split(',');
+        this.setState({ unitData: [...res] })
+    }
+
+    handleOnChangeRmaID = e => this.setState({ rmaID: e.target.value })
+
+    handleOnReset = () => this.setState({ scanUnit: "", unitData: ["", "", "", "", ""], rmaID: "" })
+    render() {
+        const { unitData } = this.state;
+        return (
+            <div style={{ textAlign: "center", marginTop: "15px", marginLeft: "50px", display: "flex" }}>
+                <Paper style={{ width: "600px" }}>
+                    <Typography variant="h3" component="h3">
+                        RMA Label Print Tool.
+        </Typography>
+                    <Typography variant="subtitle1" component="h5">
+                        Please scan a unit to print the label.
+        </Typography>
+                    <TextField
+                        id="outlined-email-input"
+                        required
+                        label="Scan a unit"
+                        type="text"
+                        name="scanUnit"
+                        margin="normal"
+                        variant="outlined"
+                        onChange={this.handleOnChange}
+                        value={this.state.scanUnit}
+                        autoFocus={true}
+                        fullWidth
+                    /><br />
+                    <Button variant="contained" color="secondary" onClick={this.handleOnReset}>
+                        Reset
+      </Button>
+                </Paper>
+                <Paper style={{ width: "400px", height: "500px", marginLeft: "5px" }}>
+                    <TextField
+                        required
+                        id="outlined-email-input"
+                        label="Serial Number"
+                        type="text"
+                        name="serial"
+                        autoComplete="serial-number"
+                        margin="normal"
+                        variant="outlined"
+                        onChange={this.handleOnChange}
+                        value={unitData[0]}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+
+                    />
+                    < br />
+                    <TextField
+                        id="outlined-email-input"
+                        label="RMA ID"
+                        type="text"
+                        name="rma-id"
+                        autoComplete="rma-id"
+                        margin="normal"
+                        variant="outlined"
+                        value={this.state.rmaID}
+                        onChange={this.handleOnChangeRmaID}
+                        required
+                    />
+                    < br />
+                    <TextField
+                        required
+                        id="outlined-email-input"
+                        label="Prod Unit"
+                        type="text"
+                        name="prod-unit"
+                        autoComplete="prod-unit"
+                        margin="normal"
+                        variant="outlined"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        value={unitData[1]}
+                    />
+                    < br />
+                    <TextField
+                        id="outlined-email-input"
+                        label="C.O.O"
+                        type="text"
+                        name="country"
+                        autoComplete="country"
+                        margin="normal"
+                        variant="outlined"
+                        value={unitData[4]}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        required
+                    />
+                    <br />
+                    <ReactToPrint
+                        copyStyles={false}
+                        trigger={() => <Button variant="contained" color="primary">
+                            Print
+  </Button>}
+                        content={() => this.componentRef}
+                    />
+
+                </Paper>
+
+                <PrintRmaLabel ref={el => (this.componentRef = el)} RMAID={this.state.rmaID} SerialNumber={unitData[0]} ProdUnit={unitData[1]} Country={unitData[4]} />
+
+
+            </div >
+        );
+    }
+}
+
+export default RmaForm;
