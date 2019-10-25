65e274998d5bad70d5d6a86b67d350e408cd4de8
Farhan-Nazir <nazirf3440@gmail.com>
2019-10-25 21:07:44 +0200

    Demo is ready

---
+++ build/precache-manifest.72febad6b8d959698907711e3c2652aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2065864f021e31877e74132634d7a472",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91f9ac9f5cbfade84506",
+    "url": "/static/css/main.e9639a40.chunk.css"
+  },
+  {
+    "revision": "34a74225a7fcb2460028",
+    "url": "/static/js/2.898167d2.chunk.js"
+  },
+  {
+    "revision": "91f9ac9f5cbfade84506",
+    "url": "/static/js/main.d9bdc100.chunk.js"
+  },
+  {
+    "revision": "235334d06b6a0bceef10",
+    "url": "/static/js/runtime-main.21f2e51c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
